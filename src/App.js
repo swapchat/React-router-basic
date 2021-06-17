08475ecec714aa1a669f50370a23ef8456aa44e3
Swapnil Chaturvedi <swapnilchaturvedi96@gmail.com>
2021-06-17 12:15:28 +0530

    Add the final project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import Header from "./components/Header";
 
-function App() {
+const Home = () => {
+  return <h2>Home Page</h2>;
+};
+
+const About = () => {
+  return <h2>About Page</h2>;
+};
+
+const Contact = () => {
+  return <h2>Contact Page </h2>;
+};
+
+const NotFound = () => {
+  return <h2>Page Not Found!</h2>;
+};
+
+const Profile = (props) => {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Profile Page</h2>
+      <Link to="/profile/10">Edit Profile</Link>
     </div>
   );
-}
+};
+
+const EditProfile = (props) => {
+  console.log(props);
+  return <h2>Editing profile page of user with id {props.match.params.id}</h2>;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/profile" component={Profile} exact={true} />
+        <Route path="/profile/:id" component={EditProfile} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <ul className="navigation-menu">
+      <li>
+        <NavLink to="/" activeClassName="active" exact={true}>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/about" activeClassName="active">
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact" activeClassName="active">
+          Contact
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/profile" activeClassName="active">
+          Profile
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
